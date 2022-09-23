271e3c3309ab4a3331db54ca2646bb6a3a45c539
mikebavon okello <bavomike@gmail.com>
2022-09-23 09:22:26 +0300

    -adding maven run command in notes.txt

--- src/main/java/com/cohort10/sys/App.java
+++ src/main/java/com/cohort10/sys/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to java basic recap for cohort 10 System. The GURUS" );
     }
 }
